11138f45d61b6bdef48964022c315dcc9acd81ea
Antonio <antoniojosedonishung@gmail.com>
2023-01-07 17:53:13 -0500

    feat: added static files to the application

---
+++ components/build/_app/immutable/chunks/2-aa7afd17.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/candles/_page.svelte-62bd03fe.js";export{t as component};
---
+++ components/build/_app/immutable/chunks/3-b63b7a31.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/peers/_page.svelte-b70c4048.js";export{t as component};
