3733b0e042ecce6c15fac9f08386a6e490c5d05b
Antonio <antoniojosedonishung@gmail.com>
2023-01-07 14:44:59 -0500

    feat: peers graph support

--- components/src/routes/peers/+page.svelte
+++ components/src/routes/peers/+page.svelte
@@ -3,7 +3,7 @@
 	import Peers from '$lib/components/peers.svelte';
 
 	let symbol: string = $page.url.searchParams.get('symbol') || 'AAPL';
-	const depthString: string = $page.url.searchParams.get('depth') || '2';
+	const depthString: string = $page.url.searchParams.get('depth') || '100';
 	let depth: number = parseInt(depthString);
 </script>
 
