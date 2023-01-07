a5aeb978968dbe7b4cc3d8263d7916fed3f08652
Antonio <antoniojosedonishung@gmail.com>
2023-01-07 17:27:01 -0500

    fix: window location based solution

---
+++ components/components.go
@@ -0,0 +1,13 @@
+package components
+
+import (
+	"embed"
+)
+
+//go:embed build/* build/_app/*
+//go:embed build/_app/immutable
+//go:embed build/_app/immutable/modules/pages/*
+//go:embed build/_app/immutable/components/pages/candles/*
+//go:embed build/_app/immutable/components/pages/peers/*
+//go:embed build/_app/immutable/chunks/*
+var Components embed.FS
