5be251c192617f1be22211653a358e14560cdcdf
Antonio <antoniojosedonishung@gmail.com>
2023-01-07 16:43:11 -0500

    fix: added support for url based query params

--- components/src/routes/candles/+page.svelte
+++ components/src/routes/candles/+page.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import Candles from '$lib/components/candles.svelte';
+	import { onMount } from 'svelte';
 
-	let symbol: string = $page.url.searchParams.get('symbol') || 'AAPL';
-	let resolution: string = $page.url.searchParams.get('resolution') || 'D';
-	const lastString: string = $page.url.searchParams.get('last') || '0';
-	let last: number = parseInt(lastString);
-	const fromString: string = $page.url.searchParams.get('from') || '1667278800000';
-	let from: number = parseInt(fromString);
-	const toString: string = $page.url.searchParams.get('to') || '1673121379603';
-	let to: number = parseInt(toString);
+	let symbol: string;
+	let resolution: string;
+	let last: number;
+	let from: number;
+	let to: number;
+
+	let loaded: boolean = false;
+
+	onMount(async () => {
+		if (window && window.location && window.location.search) {
+			const params: URLSearchParams = new Proxy(new URLSearchParams(window.location.search), {
+				get: (searchParams, prop) => searchParams.get(prop)
+			});
+			symbol = params.symbol || 'AAPL';
+			resolution = params.resolution || 'D';
+			const lastString: string = params.last || '0';
+			last = parseInt(lastString);
+			const fromString: string = params.from || '1667278800000';
+			from = parseInt(fromString);
+			const toString: string = params.to || '1673121379603';
+			from = parseInt(toString);
+
+			loaded = true;
+		}
+	});
 </script>
 
-<Candles bind:symbol bind:resolution bind:last bind:from bind:to />
+{#key loaded}
+	{#if loaded}
+		<Candles bind:symbol bind:resolution bind:last bind:from bind:to />
+	{/if}
+{/key}
