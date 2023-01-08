36aa68df853a4c048987f1cbb6c30e49b83a1dce
Antonio <antoniojosedonishung@gmail.com>
2023-01-07 21:07:22 -0500

    feat: added support for the latest components tooling

---
+++ components/build/_app/immutable/chunks/2-3d7da35d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/candles/_page.svelte-e7f17716.js";export{t as component};
--- components/build/_app/immutable/chunks/2-6d875c1c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/candles/_page.svelte-6dc18575.js";export{t as component};
