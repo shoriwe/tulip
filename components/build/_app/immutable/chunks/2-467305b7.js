e562e6dd63c1bf34979b7adea0e2b8caffbf8bfc
Antonio Donis <antoniojosedonishung@gmail.com>
2023-01-08 01:06:33 +0000

    [ci skip] generated components

--- components/build/_app/immutable/chunks/2-467305b7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/candles/_page.svelte-15a13d4f.js";export{t as component};
---
+++ components/build/_app/immutable/chunks/2-6d875c1c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/candles/_page.svelte-6dc18575.js";export{t as component};
