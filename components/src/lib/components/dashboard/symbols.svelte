6620efdb824b0486f3951594d2a8bfb7d62d9877
Antonio <antoniojosedonishung@gmail.com>
2023-01-11 18:53:57 -0500

    fix also re-render when resolution and last interval changes

--- components/src/lib/components/dashboard/symbols.svelte
+++ components/src/lib/components/dashboard/symbols.svelte
@@ -6,7 +6,7 @@
 	export let last: number;
 </script>
 
-{#key symbols}
+{#key symbols || resolution || last}
 	<div class="grid sm:grid-cols-1 md:grid-cols-3">
 		{#each symbols as symbol}
 			<SimpleCandles {symbol} bind:resolution bind:last height="275px" />
