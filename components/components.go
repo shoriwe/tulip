60017234b6c259b2271b6dc847b821b99b5320d9
Antonio <antoniojosedonishung@gmail.com>
2023-01-11 18:45:21 -0500

    fix: added dashboard components

--- components/components.go
+++ components/components.go
@@ -7,11 +7,13 @@ import (
 //go:embed build/* build/_app/*
 //go:embed build/_app/immutable
 //go:embed build/_app/immutable/modules/pages/*
+//go:embed build/_app/immutable/components/pages/_*
 //go:embed build/_app/immutable/components/pages/candles/*
 //go:embed build/_app/immutable/components/pages/peers/*
 //go:embed build/_app/immutable/components/pages/recommendation-trends/*
 //go:embed build/_app/immutable/components/pages/heatmap/*
 //go:embed build/_app/immutable/components/pages/editor/*
+//go:embed build/_app/immutable/components/pages/dashboard/*
 //go:embed build/_app/immutable/chunks/*
 //go:embed build/_app/immutable/assets/*
 var Components embed.FS
