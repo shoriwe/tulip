568ad8a56dcc39204601beb5668a32ca9a58c76c
Antonio <antoniojosedonishung@gmail.com>
2023-01-11 18:08:33 -0500

    feat: complete implementation of the dashboard

---
+++ components/src/lib/components/dashboard/notes.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Editor from '../editor/editor.svelte';
+	import { symbolsToNoteName } from '$lib/api/client';
+	import { onMount } from 'svelte';
+
+	export let symbols: string[];
+
+	let name: string;
+
+	onMount(async () => {
+		name = symbolsToNoteName(symbols);
+	});
+
+	$: {
+		symbols;
+		name = symbolsToNoteName(symbols);
+	}
+</script>
+
+<Editor bind:name />
