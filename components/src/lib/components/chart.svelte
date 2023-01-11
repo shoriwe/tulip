b90db6412217cda487692b978f41c5396eac903f
Antonio <antoniojosedonishung@gmail.com>
2023-01-11 13:52:45 -0500

    fix: responsive charts

--- components/src/lib/components/chart.svelte
+++ components/src/lib/components/chart.svelte
@@ -7,19 +7,15 @@
 	let chartDom: HTMLElement;
 	let chart: echarts.ECharts;
 
-	let innerWidth: number;
 	let innerHeight: number;
 
 	onMount(async function () {
-		chartDom.style.width = `${innerWidth - 20}px`;
-		chartDom.style.height = `${innerHeight - 20}px`;
+		chartDom.style.height = `${innerHeight - 30}px`;
 		chart = echarts.init(chartDom);
 		chart.showLoading();
 		window.onresize = function () {
-			chart.resize({
-				width: innerWidth,
-				height: innerHeight
-			});
+			chartDom.style.height = `${innerHeight - 30}px`;
+			chart.resize({height: innerHeight - 30});
 		};
 	});
 
@@ -36,5 +32,7 @@
 	}
 </script>
 
-<svelte:window bind:innerWidth bind:innerHeight />
-<div bind:this={chartDom} />
+<svelte:window bind:innerHeight />
+<div style="width: 100%;">
+	<div bind:this={chartDom} />
+</div>
