0a90fa92030655c1349403f3ca3cc6577334c5bc
Antonio <antoniojosedonishung@gmail.com>
2023-01-11 11:00:00 -0500

    feat: added flowbite UI component lib

--- components/tailwind.config.cjs
+++ components/tailwind.config.cjs
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+const config = {
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
+
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: 'class',
+};
+
+module.exports = config;
