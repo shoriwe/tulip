4a04338c68488b7fecfe4b60a5127503c6c0c9ef
Antonio <antoniojosedonishung@gmail.com>
2023-01-11 11:35:42 -0500

    feat: auto ping configuration

--- components/src/lib/api/config.ts
+++ components/src/lib/api/config.ts
@@ -1,1 +1,16 @@
-export const baseUrl: string = window? window.location.origin : 'http://127.0.0.1:5000';
+let backendUrl: string;
+try {
+    backendUrl = window.location.origin;
+
+} catch {
+    backendUrl = 'http://127.0.0.1:5000';
+}
+
+try {
+    const response = await fetch(`${backendUrl}/api/ping`);
+    if (response.status !== 200) throw 'invalid backend url';
+} catch {
+    backendUrl = 'http://127.0.0.1:5000';
+}
+
+export const baseUrl: string = backendUrl;
