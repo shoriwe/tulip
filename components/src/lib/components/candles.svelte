b581f4f1b67dcb7a9c6e3d4892dc43b071c3db41
Antonio <antoniojosedonishung@gmail.com>
2023-01-14 17:42:03 -0500

    feat: lightweight charts support for candlestick plot
    * feat: installed lightweight-charts
    
    * feat: added lightweight charts candles sticks plot
    
    * cleaner candlestick preview
    
    * fix: updated images
    
    * chore(release): 0.9.0

--- components/src/lib/components/candles.svelte
+++ components/src/lib/components/candles.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	import { candlesOptions } from '$lib/charts/candles';
+	import { candles, type Candle } from '$lib/api/client';
+	import { calcLast } from '$lib/charts/timestamp';
+	import {
+		createChart,
+		type BarData,
+		type CandlestickData,
+		type HistogramData,
+		type IChartApi,
+		type Time
+	} from 'lightweight-charts';
 	import { onMount } from 'svelte';
-	import Chart from './chart.svelte';
 
 	export let upColor: string = 'green';
 	export let downColor: string = 'red';
@@ -11,11 +19,73 @@
 	export let from: number = 0;
 	export let to: number = 0;
 
-	let option: any;
+	let chartElement: HTMLDivElement;
+	let chart: IChartApi;
+	let loaded: boolean = false;
 
 	onMount(async function () {
-		option = await candlesOptions(upColor, downColor, symbol, resolution, last, from, to);
+		if (loaded) {
+			return;
+		}
+		if (last !== 0) {
+			const fromTo = calcLast(resolution, last);
+			from = fromTo.from;
+			to = fromTo.to;
+		}
+		const cs: Candle[] = await candles(symbol, resolution, from, to);
+		const chartOptions = {
+			rightPriceScale: {
+				// positioning the price scale for the area series
+				scaleMargins: {
+					top: 0.1,
+					bottom: 0.4
+				},
+				borderVisible: false
+			}
+		};
+		chart = createChart(chartElement, chartOptions);
+		const priceSeries = chart.addCandlestickSeries();
+		priceSeries.setData(
+			cs.map((candle: Candle): CandlestickData => {
+				const candleColor: string = candle.open < candle.close ? upColor : downColor;
+				let result: CandlestickData = {
+					color: candleColor,
+					wickColor: candleColor,
+					borderColor: 'black',
+					open: candle.open,
+					close: candle.close,
+					low: candle.low,
+					high: candle.high,
+					time: candle.timestamp as Time
+				};
+				return result;
+			})
+		);
+		const volumeSeries = chart.addHistogramSeries({
+			color: '#26a69a',
+			priceFormat: {
+				type: 'volume'
+			},
+			priceScaleId: '', // set as an overlay by setting a blank priceScaleId
+			// set the positioning of the volume series
+			scaleMargins: {
+				top: 0.7, // highest point of the series will be 70% away from the top
+				bottom: 0
+			}
+		});
+		volumeSeries.setData(
+			cs.map((candle: Candle): HistogramData => {
+				const candleColor: string = candle.open < candle.close ? 'green' : 'red';
+				let result: HistogramData = {
+					color: candleColor,
+					value: candle.volume,
+					time: candle.timestamp as Time
+				};
+				return result;
+			})
+		);
+		loaded = true;
 	});
 </script>
 
-<Chart bind:option />
+<div class="w-full h-full" bind:this={chartElement} />
--- components/src/lib/components/dashboard/symbols.svelte
+++ components/src/lib/components/dashboard/symbols.svelte
@@ -9,7 +9,22 @@
 {#key symbols || resolution || last}
 	<div class="grid sm:grid-cols-1 md:grid-cols-3">
 		{#each symbols as symbol}
-			<SimpleCandles {symbol} bind:resolution bind:last height="275px" />
+			<div class="chart-preview">
+				<SimpleCandles {symbol} bind:resolution bind:last height="275px" />
+			</div>
 		{/each}
 	</div>
 {/key}
+
+<style>
+	.chart-preview {
+		padding: 5px;
+		transition: scale 0.5s;
+		margin: 5px;
+		box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+	}
+
+	.chart-preview:hover {
+		scale: 1.01;
+	}
+</style>
