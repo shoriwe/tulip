8c0442235d4e4e8450bcf7706cb5a5fdf7d240e4
Antonio Donis <antoniojosedonishung@gmail.com>
2023-01-22 03:25:33 +0000

    [ci skip] feat: generated components

--- components/build/_app/immutable/chunks/6-b63aa709.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/gains/_page.svelte-8ff9e769.js";export{t as component};
---
+++ components/build/_app/immutable/chunks/6-eca0c76d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/gains/_page.svelte-5cb8a4bf.js";export{t as component};
